39583a04cee801105bbca0cda773f8589ada39a7
Jerry Ying <59452354+ying234@users.noreply.github.com>
2023-06-01 09:05:56 -0700

    Only add clipboard when it's wsl

--- plugins/my_other_plugins.lua
+++ plugins/my_other_plugins.lua
@@ -1,15 +1,28 @@
-vim.g.clipboard = {
-  name = "wslClipboard",
-  copy = {
-    ["+"] = "clip.exe",
-    ["*"] = "clip.exe",
-  },
-  paste = {
-    ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-  },
-  cache_enable = 0,
-}
+local is_wsl = (function()
+  local output = vim.fn.systemlist "uname -r"
+  return not not string.find(output[1] or "", "WSL")
+end)()
+
+-- local is_windows = vim.fn.has("win32") == 1
+--
+-- local is_mac = vim.fn.has("macunix") == 1
+--
+-- local is_linux = not is_wsl and not is_mac
+
+if is_wsl then
+  vim.g.clipboard = {
+    name = "wslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enable = 0,
+  }
+end
 
 return {
   {
